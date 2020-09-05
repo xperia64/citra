7306cf6202e83e15de497285abd6f28bf759c5f5
xperia64 <xperiancedapps@gmail.com>
2020-09-05 18:49:42 -0400

    Initial implementation of partial_embedded_buffer_dirty handling

--- src/audio_core/hle/source.h
+++ src/audio_core/hle/source.h
@@ -137,6 +137,7 @@ private:
 
         u32 current_sample_number = 0;
         u32 next_sample_number = 0;
+        PAddr current_buffer_physical_address = 0;
         AudioInterp::StereoBuffer16 current_buffer = {};
 
         // buffer_id state
